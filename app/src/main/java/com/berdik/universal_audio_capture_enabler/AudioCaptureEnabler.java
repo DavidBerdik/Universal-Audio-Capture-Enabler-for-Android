8bbadd63e0a7e207c82af9b3d61545e514f36221
David Berdik <berdikgit@theprogrammersworld.net>
2021-09-28 20:32:30 -0400

    Fixed hooking for setAllowAudioPlaybackCapture() in ParsingPackageImpl and removed the two other hooked functions. They will be fixed and restored later.

--- app/src/main/java/com/berdik/universal_audio_capture_enabler/AudioCaptureEnabler.java
+++ app/src/main/java/com/berdik/universal_audio_capture_enabler/AudioCaptureEnabler.java
@@ -16,60 +16,22 @@ public class AudioCaptureEnabler implements IXposedHookLoadPackage {
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) {
         // Filter by package names with "android" in them and limit hooking to Android 11.
-        if (loadPackageParam.packageName.equals("android") &&
-                Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             // Bypass "allowAudioPlaybackCapture" in AndroidManifest.xml
             /* https://cs.android.com/android/platform/superproject/+/master:frameworks/
                 base/core/java/android/content/pm/parsing/ParsingPackageImpl.java;l=2539 */
             try {
-                Class<?> parsingPkgImpl = XposedHelpers.findClass(
+                XposedHelpers.findAndHookMethod(
                         "android.content.pm.parsing.ParsingPackageImpl",
-                        loadPackageParam.classLoader);
-
-                XposedHelpers.findAndHookMethod(
-                        parsingPkgImpl,
+                        loadPackageParam.classLoader,
                         "setAllowAudioPlaybackCapture",
-                        XC_MethodReplacement.returnConstant(true));
-            } catch (Throwable t) {
-                XposedBridge.log(t);
-            }
-
-            // Bypass AudioManager's "setAllowedCapturePolicy()"
-            /* https://cs.android.com/android/platform/superproject/+/master:frameworks/base/media/
-                java/android/media/AudioManager.java;l=1554?q=setAllowedCapturePolicy&start=11 */
-            try {
-                Class<?> audioManager = XposedHelpers.findClass(
-                        "android.media.AudioManager", loadPackageParam.classLoader);
-
-                XposedHelpers.findAndHookMethod(
-                        audioManager,
-                        "setAllowedCapturePolicy",
+                        boolean.class,
                         new XC_MethodHook() {
                             @Override
                             protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                                param.args[0] = AudioAttributes.ALLOW_CAPTURE_BY_ALL;
-                            }
-                        });
-            } catch (Throwable t) {
-                XposedBridge.log(t);
-            }
-
-            // Bypass AudioAttributes.Builder's "setAllowedCapturePolicy()"
-            /* https://cs.android.com/android/platform/superproject/+/master:frameworks/base/media/
-                java/android/media/AudioAttributes.java;l=879 */
-            try {
-                Class<?> audioAttribBuilder = XposedHelpers.findClass(
-                        "android.media.AudioAttributes.Builder",
-                        loadPackageParam.classLoader);
-
-                XposedHelpers.findAndHookMethod(
-                        audioAttribBuilder,
-                        "setAllowedCapturePolicy",
-                        new XC_MethodHook() {
-                            @Override
-                            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                                param.args[0] = AudioAttributes.ALLOW_CAPTURE_BY_ALL;
+                                param.args[0] = true;
+                                XposedBridge.log("Hooked android.content.pm.parsing." +
+                                        "ParsingPackageImpl#setAllowAudioPlaybackCapture()");
                             }
                         });
             } catch (Throwable t) {
